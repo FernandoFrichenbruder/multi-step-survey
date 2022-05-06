499d064fc0ea5018df8eb40011d996f7fe943e38
Fernando Frichenbruder <fernando.frich@gmail.com>
2022-05-06 12:34:45 -0300

    Remove unnecessary text

--- src/App.js
+++ src/App.js
@@ -5,17 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <p>
-          Starting Yieldstreet challenge.
+          Yieldstreet challenge.
         </p>
-
-        <p>
-          Multi-step form...
-        </p>
-
         <Form />
-        
       </header>
     </div>
   );
