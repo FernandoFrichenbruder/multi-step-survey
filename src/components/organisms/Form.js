9bf8520bedeb80515d8531b2b5a940985dab76ba
Fernando Frichenbruder <fernando.frich@gmail.com>
2022-05-06 10:31:43 -0300

    Add Multi-step logic

--- src/components/organisms/Form.js
+++ src/components/organisms/Form.js
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Identify from '../molecules/Identify'
 import Details from '../molecules/Details'
 import Favorites from '../molecules/Favorites'
 import Summary from '../molecules/Summary'
 
 function Form() {
-  return (
-    <div className='form'>
-        <header>
-            <p>Some title goes here</p>
-        </header>
-        <section>
-            <p>Molecules goes here</p>
-        </section>
-        <footer>
-            <p>Buttons goes here</p>
-        </footer>
-    </div>
-  )
+
+    const [page, setPage] = useState(0);
+
+    const SurveyStep = () => {       
+        switch ( page ) {
+            case 0:
+                return <Identify />
+            case 1:
+                return <Details />
+            case 2:
+                return <Favorites />
+            case 3:
+                return <Summary />
+            default:
+                return <p>Ops, something went wrong</p> //Create some default
+        }
+    }
+
+    return (
+        <div className='form'>
+            <header>
+                <p>Some title goes here</p>
+            </header>
+            <section>
+                <SurveyStep />
+            </section>
+            <footer>
+
+                <button
+                    disabled={page === 0}
+                    onClick={() => {setPage((currentPage) => currentPage - 1)}}
+                >
+                    Prev
+                </button>
+
+                <button
+                    disabled={page === 3}
+                    onClick={() => {setPage((currentPage) => currentPage + 1)}}
+                >
+                    Next
+                </button>
+
+            </footer>
+        </div>
+    )
 }
 
 export default Form
