d94bb79586a3e00557591194c77c33af58407428
Fernando Frichenbruder <fernando.frich@gmail.com>
2022-05-06 10:14:26 -0300

    Create semantics for the form component

--- src/components/organisms/Form.js
+++ src/components/organisms/Form.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Identify from '../molecules/Identify'
+import Details from '../molecules/Details'
+import Favorites from '../molecules/Favorites'
+import Summary from '../molecules/Summary'
 
 function Form() {
   return (
-    <div>Form</div>
+    <div className='form'>
+        <header>
+            <p>Some title goes here</p>
+        </header>
+        <section>
+            <p>Molecules goes here</p>
+        </section>
+        <footer>
+            <p>Buttons goes here</p>
+        </footer>
+    </div>
   )
 }
 
