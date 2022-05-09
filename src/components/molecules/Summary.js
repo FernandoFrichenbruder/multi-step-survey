9a9468e40b5b6cb0d1f0b628afe62a92f293d29f
Fernando Frichenbruder <fernando.frich@gmail.com>
2022-05-08 23:39:20 -0300

    Add Summary return from state

--- src/components/molecules/Summary.js
+++ src/components/molecules/Summary.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-function Summary() {
+function Summary({getState}) {
+  const table = getState()
+  console.log(table)
   return (
-    <div>Summary</div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+
+
+          {table && Object.keys(table).map((key, value) => (
+            <TableRow
+              key={key}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {key}
+              </TableCell>
+              <TableCell align="right">{table[key]}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 
--- src/components/organisms/Form.js
+++ src/components/organisms/Form.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Stack from '@mui/material/Stack'
 import Step  from '../molecules/Step'
+import Summary from '../molecules/Summary'
 
 export default class Form extends React.Component {
     constructor(props) {
@@ -25,6 +26,10 @@ export default class Form extends React.Component {
         return this.state.formData[key]
     }
 
+    getState = () => {
+        return this.state.formData
+    }
+
 
     async componentDidMount() {
         const response = await fetch('mock.json', {headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }});
@@ -36,11 +41,16 @@ export default class Form extends React.Component {
             this.setState(prevState => ({
                 surveySteps: [...prevState.surveySteps, <Step title={e.title} inputs={[...e.inputs]} setFormDataState={this.setFormDataState} getFormDataState={this.getFormDataState} /> ]
              }))
+             
         })
+        this.setState(prevState => ({
+            surveySteps: [...prevState.surveySteps, <Summary getState={this.getState} /> ]
+         }))
 
     }
         
     render() {
+        console.log('data ' + JSON.stringify(this.state.formData))
         return (
             <Box>
                 {this.state.surveySteps[this.state.page]}
